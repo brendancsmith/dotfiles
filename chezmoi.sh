277b8bd1ffc0728fe0a54fea4584c61ae9afa6f0
Brendan Smith <brendan.smith.93@gmail.com>
2024-07-06 09:58:10 -0500

    Install chezmoi

--- chezmoi.sh
+++ chezmoi.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env sh
 
-echo "--- CHEZMOI ---"
+# -e: exit on error
+# -u: exit on unset variables
+set -eu
+
+if ! command -v chezmoi >/dev/null 2>&1; then
+  brew install chezmoi
+fi
+
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+# shellcheck disable=SC2312
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+
+# exec: replace current process with chezmoi init
+log_task "Running 'chezmoi $*'"
+exec "chezmoi init --apply --source=${script_dir}"
