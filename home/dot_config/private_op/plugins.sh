cc942a48d1b5b9e2f86da71e177a8df3e080b29f
Brendan Smith <mail@brendansmith.ai>
2024-10-20 11:37:22 -0500

    Only source op plugins once

--- home/dot_config/private_op/plugins.sh
+++ home/dot_config/private_op/plugins.sh
@@ -1,3 +1,8 @@
-export OP_PLUGIN_ALIASES_SOURCED=1
-alias gh="op plugin run -- gh"
-alias openai="op plugin run -- openai"
+#!/usr/bin/env sh
+
+if [ -z "$OP_PLUGIN_ALIASES_SOURCED" ]; then
+	export OP_PLUGIN_ALIASES_SOURCED=1
+	alias gh="op plugin run -- gh"
+	alias openai="op plugin run -- openai"
+	alias aws="op plugin run -- aws"
+fi
